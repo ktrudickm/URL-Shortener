96935a9176360c43e24e71c40346af68af01b813
Kendall <ktrudickm@gmail.com>
2022-05-09 16:53:52 -0400

    Package name rename

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Navbar from '../components/NavBar/NavBar';
 import SideBar from '../components/SideBar/SideBar'
 import MainPage from '../components/Main/Main'
-import Footer from '../components/Footer/Footer'
 import BoostSection from '../components/BoostSection/Boost'
 import Stats from '../components/StatsSection/Stats'
 import Form from '../components/FormField/Form'
